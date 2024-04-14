34ff19b26c9f16917a97db415ffdf2c18dd7ba05
Javi Martín <javim@elretirao.net>
2024-04-14 20:45:01 +0200

    Rails 7.1 TODO: Apartment can't add schemas due to PR 49164 in https://github.com/rails/rails/
    We're disabling `action_controller.raise_on_missing_callback_actions`
    because sometimes we include `before_action :something, only: actions`
    in concerns, and we include these concerns in controllers that don't
    have all these actions.
    
    Note that Rails 7.1 logs to STDOUT by default [1]; we're re-adding the
    condition `if ENV["RAILS_LOG_TO_STDOUT"].present?` because we're still
    using files and we're rotating the logs to avoid running out of space.
    
    Also note that, since now the `shared_extensions` schema is added to the
    `schema.rb` file, we need to make sure we move extensions to that schema
    _after_ the `db:schema:load` task has finished.
    
    [1] Pull request 47138 in https://github.com/rails/rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_24_013913) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_24_013913) do
+  create_schema "shared_extensions"
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
