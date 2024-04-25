9ed9d23a5bbf50782f22b7715cb5013c31a6d0ef
Javi Martín <javim@elretirao.net>
2024-04-26 00:41:26 +0200

    Remove the campaigns table
    We stopped using the Campaign model in commit 448775a5e.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_22_223950) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_25_223908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -403,13 +403,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_22_223950) do
     t.string "voting_style", default: "knapsack"
     t.boolean "published"
     t.boolean "hide_money", default: false
-  end
-
-  create_table "campaigns", id: :serial, force: :cascade do |t|
-    t.string "name"
-    t.string "track_id"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "ckeditor_assets", id: :serial, force: :cascade do |t|
