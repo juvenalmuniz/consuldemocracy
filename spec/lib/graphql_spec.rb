46d1825a90daa9248a8d16ffdb04bef15cac7299
cyrillefr <cyrille.vasseur@gmail.com>
2024-07-02 15:47:42 +0200

    Milestone type added
    - added the milestone type to be displayed with investments
    - the corresponding spec

--- spec/lib/graphql_spec.rb
+++ spec/lib/graphql_spec.rb
@@ -678,6 +678,16 @@ describe "Consul Schema" do
     end
   end
 
+  describe "Milestone" do
+    it "formats publication date like in view" do
+      milestone = create(:milestone, publication_date: Time.zone.parse("2024-07-02 11:45:17"))
+
+      response = execute("{ milestone(id: #{milestone.id}) { id date_of_publication } }")
+      received_date_of_publication = dig(response, "data.milestone.date_of_publication")
+      expect(received_date_of_publication).to eq "2024-07-02"
+    end
+  end
+
   describe "Budget investment" do
     it "does not include hidden comments" do
       budget = create(:budget)
