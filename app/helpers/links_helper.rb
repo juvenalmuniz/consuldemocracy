72365d3ee66a974f7b22c7ce0a847a9a4debce07
Juvenal Junior da Silva Muniz <juvenal.muniz@ufms.br>
2024-10-04 13:51:09 -0400

    Revert "Mais links diretos para sign in"
    This reverts commit 60475d746f500dc0402a13228873c2eee4bf0de8.

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -6,8 +6,7 @@ module LinksHelper
   end
 
   def link_to_signin(options = {})
-    link_to t("devise_views.menu.login_items.login"),
-            user_keycloak_openid_omniauth_authorize_path, method: :post, options: options
+    link_to t("users.signin"), new_user_session_path, options
   end
 
   def link_to_signup(options = {})
