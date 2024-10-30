8a1e6429093d4998c314aaf79fb085aa1689e235
Juvenal Junior da Silva Muniz <juvenal.muniz@ufms.br>
2024-10-30 14:34:59 -0400

    Revert "Remoção do limite de 5 apoios"
    This reverts commit d87385e87dd1e8fb77106828e237048edc150dca.

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -26,6 +26,8 @@ class ProposalsController < ApplicationController
   helper_method :resource_model, :resource_name
   respond_to :html, :js
 
+  MAX_VOTES = 5
+
   def show
     super
     @notifications = @proposal.notifications
@@ -58,6 +60,7 @@ class ProposalsController < ApplicationController
 
   def vote
     value = params[:value]
+    @max_votes_reached = current_user.votes.count >= MAX_VOTES && value == 'yes'
     @follow = Follow.find_or_create_by!(user: current_user, followable: @proposal)
     @proposal.register_vote(current_user, value)
   end
--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -186,7 +186,7 @@ class Proposal < ApplicationRecord
     if votable_by?(user) && !archived?
       if (vote_value == 'no')
         unvote_by(user)
-      else
+      elsif user&.votes.distinct.count(:votable_id) < 5
         vote_by(voter: user, vote: vote_value)
       end
     end
