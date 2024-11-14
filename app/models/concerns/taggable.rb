002972e71ab2321e10a8c12a9a2b0f3f4a0a6848
Juvenal Junior da Silva Muniz <juvenal.muniz@ufms.br>
2024-11-14 14:31:49 -0400

    Aumentando as tags para máx. 50

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -21,6 +21,6 @@ module Taggable
   end
 
   def max_number_of_tags
-    errors.add(:tag_list, :less_than_or_equal_to, count: 6) if tag_list.count > 6
+    errors.add(:tag_list, :less_than_or_equal_to, count: 50) if tag_list.count > 50
   end
 end
